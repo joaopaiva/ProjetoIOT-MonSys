541b69715917760d540f0a60a83176f5068b805f
willerrodrigo <willerrodrigo@2006.hotmail.com>
2018-10-15 18:46:30 -0300

    Database Interaction

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDnB_7cgMO9Do9i0mJTGPVh9zdOSW20H-A",
+    authDomain: "curso-fb-e885e.firebaseapp.com",
+    databaseURL: "https://curso-fb-e885e.firebaseio.com",
+    projectId: "curso-fb-e885e",
+    storageBucket: "curso-fb-e885e.appspot.com",
+    messagingSenderId: "286547666299"
+};
+firebase.initializeApp(config);
